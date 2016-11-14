3819713430b560b9c2a0c25d81643aebd2507e6b
Patrik Ohlson <patrik@150up.com>
2016-11-14 10:14:28 +0100

    Missing file

---
+++ form.php
@@ -0,0 +1,5 @@
+		 <form method="post" action="create.php">
+<p>Input URL (with http://): <br /> <input type="text" name="url" size="50" value="http://"  /><br /><br />
+Custom tag (optional) <br /> <input type="text" name="tag" size="50" value="" /><br /><br />
+<input type="submit" name="send" value="Create short link!" class="button" /></p>
+      </form>
